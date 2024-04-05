bb0f33b72cc4063e7e40618c9d6d301070166277
Gavin LI <gavinli@Gavins-iMac.fios-router.home>
2024-04-05 15:30:37 -0400

    more

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,11 +1,10 @@
 //function CoreConcept(props)
-export default function CoreConcept({image, title, description})
- {
-    return (
-      <li>
-        <img src={image} alt={title}/>
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </li>
-    )
- }
+export default function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
+}
